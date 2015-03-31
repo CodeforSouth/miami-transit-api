e22fb1c9026b556ece73f3350aaf6c243ffe8c75
Chris Scott <cyberstrike@gmail.com>
2015-03-30 20:29:35 -0400

    added pages and index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'pages/index'
+
+  root 'pages#index'
+
   get 'buses' => 'miami_dade_transit#buses'
   get 'bus/:id' => 'miami_dade_transit#bus'
 
@@ -7,7 +11,6 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
