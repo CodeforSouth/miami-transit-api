14ee1508cc3fcd551cf971bc7afc14875d43541a
Bryce Kerley <bkerley@brycekerley.net>
2015-04-27 20:50:26 -0400

    fail gracefully if raygun doesn’t load

--- config/initializers/raygun.rb
+++ config/initializers/raygun.rb
@@ -1,7 +1,9 @@
-Raygun.setup do |config|
-  config.api_key = "plRmFuTaBd0VIiJvudaHig=="
-  config.filter_parameters = Rails.application.config.filter_parameters
+if defined? Raygun
+  Raygun.setup do |config|
+    config.api_key = "plRmFuTaBd0VIiJvudaHig=="
+    config.filter_parameters = Rails.application.config.filter_parameters
 
-  # The default is Rails.env.production?
-  # config.enable_reporting = !Rails.env.development? && !Rails.env.test?
+    # The default is Rails.env.production?
+    # config.enable_reporting = !Rails.env.development? && !Rails.env.test?
+  end
 end
